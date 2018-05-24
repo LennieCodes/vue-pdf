52b9c67cd2f4402d3f1b272cf72486f447508fc1
Lenny <lmpinskiy@uniclarity.com>
2018-05-24 14:41:36 -0400

    Commented out pdf.setCanvasHeight because of issues with resizesensor

--- src/componentFactory.js
+++ src/componentFactory.js
@@ -48,11 +48,11 @@ export default function(pdfjsWrapper) {
 		},
 		watch: {
 			src: function() {
-				
+
 				this.pdf.loadDocument(this.src);
 			},
 			page: function() {
-				
+
 				this.pdf.loadPage(this.page, this.rotate);
 			},
 			rotate: function() {
@@ -61,21 +61,21 @@ export default function(pdfjsWrapper) {
 		},
 		methods: {
 			resize: function(size) {
-		
+
 				// check if the element is attached to the dom tree || resizeSensor being destroyed
 				if ( this.$el.parentNode === null || (size.width === 0 && size.height === 0) )
 					return;
 
 				// on IE10- canvas height must be set
-				this.pdf.setCanvasHeight(this.pdf.getCanvas().offsetWidth * (this.pdf.getCanvas().height / this.pdf.getCanvas().width) + 'px');
+				// this.pdf.setCanvasHeight(this.pdf.getCanvas().offsetWidth * (this.pdf.getCanvas().height / this.pdf.getCanvas().width) + 'px');
 				// update the page when the resolution is too poor
 				var resolutionScale = this.pdf.getResolutionScale();
-				
+
 				if ( resolutionScale < 0.85 || resolutionScale > 1.15 )
 				{
 					this.pdf.renderPage(this.rotate);
 				}
-					
+
 
 				this.$refs.annotationLayer.style.transform = 'scale('+resolutionScale+')';
 			},
@@ -89,22 +89,22 @@ export default function(pdfjsWrapper) {
 		mounted: function() {
 
 			this.pdf = new PDFJSWrapper(this.$refs.canvasParent, this.$refs.annotationLayer, this.$emit.bind(this));
-			
+
 			this.$on('loaded', function() {
-				
+
 				this.pdf.loadPage(this.page, this.rotate);
 			});
-			
+
 			this.$on('page-size', function(width, height) {
 				this.pdf.setCanvasHeight(this.pdf.getCanvas().offsetWidth  * (height / width) + 'px');
 			});
-			
+
 			this.pdf.loadDocument(this.src);
 		},
 
 		// doc: destroyed hook is not called during server-side rendering.
 		destroyed: function() {
-			
+
 			this.pdf.destroy();
 		}
 	}
